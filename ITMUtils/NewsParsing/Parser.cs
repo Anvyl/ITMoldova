0afc62fd258932aca88c34e80a963f36871ecd0a
aodpi <balan.valeriu@live.com>
2016-03-13 20:33:10 +0200

    save

--- ITMUtils/NewsParsing/Parser.cs
+++ ITMUtils/NewsParsing/Parser.cs
@@ -4,11 +4,9 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Windows.Data.Xml.Dom;
 using System.Text.RegularExpressions;
-<<<<<<< HEAD
 using System.Linq;
 using System.Collections.ObjectModel;
-=======
->>>>>>> refs/remotes/origin/master
+
 
 namespace ITMUtils.NewsParsing
 {
